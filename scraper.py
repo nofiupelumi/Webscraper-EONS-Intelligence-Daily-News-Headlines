08d3cefd3dc9c63657530739d848c04168435e11
Moruf Nofiu <nofiumoruf17@gmail.com>
2025-01-19 12:54:24 +0100

    testing

--- scraper.py
+++ scraper.py
@@ -68,10 +68,10 @@ case_situation_keywords = ['victims', 'victim', 'injured']
 # Chrome options
 chrome_options = Options()
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-gpu")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--disable-gpu")
 chrome_options.binary_location = "/usr/bin/google-chrome"  # Ensure this is the correct path
 
 # Initialize WebDriver

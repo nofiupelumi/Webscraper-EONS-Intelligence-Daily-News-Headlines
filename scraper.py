02ecd48333bf5212cff29794c197a97cf8c3fff2
Moruf Nofiu <nofiumoruf17@gmail.com>
2025-01-20 16:49:26 +0100

    testing

--- scraper.py
+++ scraper.py
@@ -252,7 +252,7 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.binary_location = "/usr/bin/google-chrome"
-options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+# options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
 
 options.add_argument("--proxy-server=http://your-proxy-server:port")
 
